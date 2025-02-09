378f1ebebeeade52da17bed75612d74f6bd64f45
Niizix <123569790+Niziix@users.noreply.github.com>
2025-02-09 13:17:18 +0100

    Update hardhat.config.js

--- The_vault/hardhat.config.js
+++ The_vault/hardhat.config.js
@@ -2,11 +2,12 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
+  defaultNetwork: "sepolia",
   solidity: "0.8.20",
   networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]  // Assurez-vous qu'il n'y a pas d'erreur ici
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY], // Assurez-vous que la clé privée est correctement définie dans .env
     }
   }
 };
